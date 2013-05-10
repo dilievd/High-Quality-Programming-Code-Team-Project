90386c9ff94491873932f23ef00f645ac6a8d650
Maria Barokova <bulsite@gmail.com>
2013-05-10 13:37:20 +0300

    Add namespace for enum Direction

--- Direction.cs
+++ Direction.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public enum Direction 
-{ 
-    Up, 
-    Down, 
-    Left, 
-    Right 
+﻿namespace Labyrinth
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 }
