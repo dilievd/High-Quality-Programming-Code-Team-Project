75f70803ff05b2038df853308faebc70ab032dad
stefanpu <stefanpu13@gmail.com>
2013-05-10 13:47:16 +0300

    Adding ConsoleOutput class
    Add new class to print the messages, labyrint and highscores on the
    console screen.

---
+++ ConsoleOutput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labyrinth
+{
+    internal static class ConsoleOutput
+    {
+        internal static void Print(string info) 
+        {
+            Console.Write(info);
+        }
+
+        internal static void Print(string info, string newLineCharacter)
+        {           
+            Console.Write(info + newLineCharacter);
+        }
+    }
+}
