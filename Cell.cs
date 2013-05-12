8d424aeb1783c533d2b0d714cf04212739e16170
Maria Barokova <bulsite@gmail.com>
2013-05-12 15:21:35 +0300

    Encapsulate row, column and symbol fields

--- Cell.cs
+++ Cell.cs
@@ -8,21 +8,21 @@ namespace Labyrinth
         public const char WALL_CELL = 'X';
         public const char PLAYER = '*';
 
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public char Symbol { get; set; }
+        private int row;
+        private int column;
+        private char symbol;
 
         public Cell(int row, int column)
         {
             this.Row = row;
             this.Column = column;
-            this.Symbol = Cell.PLAYER;
+            this.symbol = Cell.PLAYER;
         }
 
-        public Cell(int row, int column, char symbol) 
+        public Cell(int row, int column, char symbol)
             : this(row, column)
         {
-            this.Symbol = symbol;
+            this.symbol = symbol;
         }
 
         public Cell(int row, int column, int randomValue)
@@ -30,17 +30,73 @@ namespace Labyrinth
         {
             if (randomValue == 0)
             {
-                this.Symbol = Cell.EMPTY_CELL;
+                this.symbol = Cell.EMPTY_CELL;
             }
             else
             {
-                this.Symbol = Cell.WALL_CELL;
+                this.symbol = Cell.WALL_CELL;
+            }
+        }
+
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+
+            set
+            {
+                if (value < 0 || LabyrinthEngine.LABYRINTH_SIZE <= value)
+                {
+                    throw new ArgumentOutOfRangeException(string.Format("Row number must be in range [0..{0}]", LabyrinthEngine.LABYRINTH_SIZE - 1));
+                }
+
+                this.row = value;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return this.column;
+            }
+
+            set
+            {
+                if (value < 0 || LabyrinthEngine.LABYRINTH_SIZE <= value)
+                {
+                    throw new ArgumentOutOfRangeException(string.Format(
+                        "Column number must be in range [0..{0}]", LabyrinthEngine.LABYRINTH_SIZE - 1));
+                }
+
+                this.column = value;
+            }
+        }
+
+        public char Symbol
+        {
+            get
+            {
+                return this.symbol;
+            }
+
+            private set
+            {
+                if (!(value == EMPTY_CELL || value == WALL_CELL || value == PLAYER))
+                {
+                    throw new ArgumentException(string.Format(
+                        "Symbol could not be {0}, it must be {1} or {2} or {3}, ", value, EMPTY_CELL, WALL_CELL, PLAYER));
+                }
+
+                this.symbol = value;
             }
         }
 
         public bool IsEmpty()
         {
-            if(this.Symbol == EMPTY_CELL)
+            if (this.Symbol == EMPTY_CELL)
             {
                 return true;
             }
